41188ad05fccdfaa728772d5611e9c2a55d3e3b1
Dustin Andrews <51100082+dbandrews@users.noreply.github.com>
2023-11-12 22:14:55 -0700

    Feat: comment scrape (#3)
    * Add script to create llama2 formatted dataset
    
    * Fix for newlines
    
    * Add sft trainer example
    
    * Add jsonl datset loading, mlflow setup
    
    * SFT fine tune revisions
    
    * Add fixes to SFT script
    
    * Add comments extraction code

--- hottakes/scraper.py
+++ hottakes/scraper.py
@@ -1,10 +1,13 @@
 # %%
-import requests
-from bs4 import BeautifulSoup
 import re
+import time
+import json
+
+import requests
 import pandas as pd
 import numpy as np
-import time
+from bs4 import BeautifulSoup
+from tenacity import retry, stop_after_attempt, wait_fixed
 from tqdm import tqdm
 from joblib import Parallel, delayed
 
@@ -88,13 +91,54 @@ def get_article_details(url):
     return dict
 
 
+# %%
+def get_comment_votes(url):
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the webpage
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # Find all div elements with a class name starting with "cmcont"
+        divs = soup.find_all("div", class_="cmcont")
+
+        # Iterate through the found div elements
+        results = []
+        for div in divs:
+            result = {}
+            result["comment_id"] = div.get("id")
+            result["url"] = url
+            result["comment_text"] = div.find(class_="comtext").get_text(strip=True)
+            # Find the child div with class "pcp" within the current div element
+            pcp_div = div.find("div", class_="pcp")
+
+            # Find the span with class "pcu" within the pcp_div
+            pcu_span = pcp_div.find("span", class_="pcu") if pcp_div else None
+
+            # Get the text content of the pcu_span, which is the value of "pcu"
+            pcu_value = pcu_span.get_text(strip=True) if pcu_span else None
+
+            # Find the span with class "pcd" within the pcp_div
+            pcd_span = pcp_div.find("span", class_="pcd") if pcp_div else None
+
+            # Get the text content of the pcd_span, which is the value of "pcd"
+            pcd_value = pcd_span.get_text(strip=True) if pcd_span else None
+
+            # Print the extracted values
+            result["pcu_value"] = pcu_value
+            result["pcd_value"] = pcd_value
+            results.append(result)
+        return results
+
 if __name__ == "__main__":
     # Settings
     years = list(range(2023, 1999, -1))
     months = list(range(12, 0, -1))
     sleep_length = 0.1
 
-    # Article Links
+    # _______________Article Links _______________
     article_urls = []
     for year in tqdm(years, desc="Years"):
         for month in months:
@@ -104,7 +148,7 @@ if __name__ == "__main__":
     pd.DataFrame(article_urls).to_csv("article_urls.csv")
     article_urls = pd.read_csv("article_urls.csv", index_col=None, names=["url"], header=0).url.to_list()
 
-    # Article Details
+    # # _______________Article Details_______________
     articles = []
     n_jobs = 8
     url_chunks = np.array_split(article_urls, n_jobs)
@@ -122,4 +166,40 @@ if __name__ == "__main__":
     results = [result for job_result in results for result in job_result]
     df_details = pd.DataFrame(results)
     df_details.columns = df_details.columns.str.lower().str.replace(" ", "_")
-    df_details.to_csv("article_details.csv", index=False)
+    df_details = (
+        df_details.replace("N/A", np.nan)
+        .dropna()
+        .assign(title_article_text=lambda _df: (_df.title + ": " + _df.article_text))
+    )
+    df_details.to_json("article_details.json", orient="records")
+
+    # _______________Article Comments_______________
+    comments = []
+    n_jobs = 8
+    article_urls = pd.read_csv("article_urls.csv", index_col=None, names=["url"], header=0).url.to_list()
+    url_chunks = np.array_split(article_urls, n_jobs)
+
+    def get_comment_votes_parallel(url_chunk, job_num, sleep_length=1):
+        details = []
+        for url in tqdm(url_chunk, desc=f"Job {job_num}", position=job_num, leave=False):
+            try:
+                details.append(get_comment_votes(url))
+            except Exception as e:
+                print(e)
+            time.sleep(sleep_length)
+        return details
+    
+    results = Parallel(n_jobs=n_jobs)(
+        delayed(get_comment_votes_parallel)(url_chunk, job_num) for job_num, url_chunk in enumerate(url_chunks)
+    )
+    # Remove empty lists from chunks
+    results = [result for job_result in results for result in job_result if result]
+    # Flatten list of lists
+    results = [result for job_result in results for result in job_result]
+    
+    with open("article_comments.json", "w") as f:
+        json.dump(results, f)
+
+    df_comments = pd.DataFrame(results)
+    df_comments.columns = df_comments.columns.str.lower().str.replace(" ", "_")
+    df_comments.to_json("article_comments.json", orient="records")
