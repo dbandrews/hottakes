40755a6ad516af045b125b3e85854f785b81591a
Dustin Andrews <dustin.brown.andrews@gmail.com>
2024-02-15 21:08:13 -0700

    Start improvement of modal inference with merged model

--- hottakes/modal_inference.py
+++ hottakes/modal_inference.py
@@ -2,8 +2,8 @@ import os
 from modal import Image, Secret, Stub, method, web_endpoint
 
 MODEL_DIR = "/model"
-PEFT_MODEL = "dbandrews/mistral-v2-dpo-227c0f16-9588-4282-9bf9-6d057c254b0c"
-BASE_MODEL = "mistralai/Mistral-7B-v0.1"
+# PEFT_MODEL = "dbandrews/mistral-v2-dpo-227c0f16-9588-4282-9bf9-6d057c254b0c"
+BASE_MODEL = "dbandrews/mistral-v3-dpo-db20c9b7-8db0-4937-a8b6-65ff4aa77ebf-merged"
 
 
 def download_model_to_folder():
@@ -18,27 +18,32 @@ def download_model_to_folder():
         token=os.environ["HUGGINGFACE_TOKEN"],
     )
 
-    snapshot_download(
-        PEFT_MODEL,
-        local_dir=MODEL_DIR,
-        token=os.environ["HUGGINGFACE_TOKEN"],
-    )
+    # snapshot_download(
+    #     PEFT_MODEL,
+    #     local_dir=MODEL_DIR,
+    #     token=os.environ["HUGGINGFACE_TOKEN"],
+    # )
     move_cache()
 
 
 image = (
     Image.from_registry("nvcr.io/nvidia/pytorch:22.12-py3")
-    .pip_install("transformers==4.35.0")
-    .pip_install("peft==0.5.0")
-    .pip_install("trl==0.7.2")
-    .pip_install("typing-inspect==0.8.0")
-    .pip_install("typing_extensions==4.5.0")
-    .pip_install("accelerate==0.23.0")
-    .pip_install("bitsandbytes==0.41.1")
+    # .pip_install("transformers==4.35.0")
+    # .pip_install("peft==0.5.0")
+    # .pip_install("trl==0.7.2")
+    # .pip_install("typing-inspect==0.8.0")
+    # .pip_install("typing_extensions==4.5.0")
+    # .pip_install("accelerate==0.23.0")
+    # .pip_install("bitsandbytes==0.41.1")
+    .pip_install(
+        "vllm==0.2.5",
+        "huggingface_hub==0.19.4",
+        "hf-transfer==0.1.4",
+        "torch==2.1.2",
+    )
     # Use the barebones hf-transfer package for maximum download speeds. No progress bar, but expect 700MB/s.
-    .pip_install("hf-transfer~=0.1")
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-    .run_function(
+    # .pip_install("hf-transfer~=0.1")
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"}).run_function(
         download_model_to_folder,
         secrets=[Secret.from_name("huggingface-secret")],
         timeout=60 * 20,
@@ -55,17 +60,10 @@ stub = Stub("hottakes-inference", image=image)
 )
 class HottakesModel:
     def __enter__(self):
-        import torch
-        from peft import AutoPeftModelForCausalLM
-        from transformers import AutoTokenizer
+        from vllm import LLM
 
-        self.model = AutoPeftModelForCausalLM.from_pretrained(
-            PEFT_MODEL,
-            low_cpu_mem_usage=True,
-            torch_dtype=torch.float16,
-            load_in_8bit=True,
-        )
-        self.tokenizer = AutoTokenizer.from_pretrained(PEFT_MODEL)
+        # Load the model. Tip: MPT models may require `trust_remote_code=true`.
+        self.llm = LLM(MODEL_DIR)
         self.template = """### Instruction:
 Use the article title and text below, to write the funniest possible comment about this article.
 
@@ -79,20 +77,32 @@ Use the article title and text below, to write the funniest possible comment abo
     # @method()
     # To deploy web endpoint, uncomment the following line
     @web_endpoint()
-    def generate(self, title_article_text: str):
-        inputs = self.tokenizer(
-            self.template.format(title_article_text=title_article_text), return_tensors="pt", truncation=True
-        )
-        inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
-        outputs = self.model.generate(**inputs, max_new_tokens=200, do_sample=True, top_p=0.9, temperature=0.9)
+    def generate(self, list_of_title_article_text: list[str]) -> list[str]:
+        """Generate comments for a list of title + article text.
 
-        generated_response = self.tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][
-            len(
-                self.template.format(title_article_text=title_article_text),
-            ) :
-        ]
-        print(f"Generated response:\n{generated_response}")
-        return generated_response
+        Parameters
+        ----------
+        list_of_title_article_text : list[str]
+            List of title + " " + article text strings.
+
+        Returns
+        -------
+        list[str]
+            Comments for each article.
+        """
+        from vllm import SamplingParams
+
+        prompts = [self.template.format(title_article_text=article) for article in list_of_title_article_text]
+
+        sampling_params = SamplingParams(
+            temperature=1.0,
+            top_p=1,
+            max_tokens=800,
+            # presence_penalty=1.15,
+        )
+
+        results = self.llm.generate(prompts, sampling_params)
+        return results
 
 
 # Test locally with `modal run hottakes/modal_inference.py`
@@ -100,7 +110,7 @@ Use the article title and text below, to write the funniest possible comment abo
 # def main():
 #     model = HottakesModel()
 #     model.generate.remote(
-#         title_article_text="Replay: Crankworx Whistler - SRAM Canadian Open Enduro Presented by Specialized - Pinkbike:   Related ContentKing and Queen of Crankworx World Tour: Current StandingsPhoto Epic: Crankworx Whistler - SRAM Canadian Open Enduro presented by SpecializedVideo: The Ever Changing Game - EWS Whistler, One Minute Round UpResults: Crankworx Whistler - SRAM Canadian Open Enduro presented by SpecializedPhoto Epic: Bringing Back the Fun - EWS Whistler, PracticeVideo: Top of the World into Khyber - EWS Whistler, Track RideVideo: Different Winners at Every Round - EWS Whistler, IntroMENTIONS: @officialcrankworx / @SramMedia / @Specialized / @WhistlerMountainBikePark / @EnduroWorldSeries  "
+#         list_of_title_article_text=["Replay: Crankworx Whistler - SRAM Canadian Open Enduro Presented by Specialized - Pinkbike:   Related ContentKing and Queen of Crankworx World Tour: Current StandingsPhoto Epic: Crankworx Whistler - SRAM Canadian Open Enduro presented by SpecializedVideo: The Ever Changing Game - EWS Whistler, One Minute Round UpResults: Crankworx Whistler - SRAM Canadian Open Enduro presented by SpecializedPhoto Epic: Bringing Back the Fun - EWS Whistler, PracticeVideo: Top of the World into Khyber - EWS Whistler, Track RideVideo: Different Winners at Every Round - EWS Whistler, IntroMENTIONS: @officialcrankworx / @SramMedia / @Specialized / @WhistlerMountainBikePark / @EnduroWorldSeries  "]
 #     )
 
 
@@ -119,7 +129,7 @@ if __name__ == "__main__":
 
     start_time = time.time()
     generation = requests.get(
-        "https://dbandrews--hottakes-inference-hottakesmodel-generate.modal.run?title_article_text="
+        "https://dbandrews--hottakes-inference-hottakesmodel-generate.modal.run?list_of_title_article_text="
         + urllib.parse.quote_plus(" ".join(sample["title_article_text"].split()[:300]))
     ).json()
     print(f"Generation took {time.time() - start_time} seconds")
